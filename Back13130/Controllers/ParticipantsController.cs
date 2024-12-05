8fe7524f307a49728f810081bd0b9e20294018cc
00013130wiut <00013130wiut1@gmail.com>
2024-12-05 09:48:57 +0500

    added participants controller

---
+++ Back13130/Controllers/ParticipantsController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Back13130.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParticipantsController : ControllerBase
+    {
+        private readonly IParticipantRepository _participantRepository;
+     
+
+        public ParticipantsController(IParticipantRepository participantRepository, IEventRepository eventRepository)
+        {
+            _participantRepository = participantRepository;
+            
+        }
+
+        // Get all participants for a specific event
+        [HttpGet("{eventId}")]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetParticipantsByEventId(int eventId)
+        {
+            var participants = await _participantRepository.GetParticipantsByEventIdAsync(eventId);
+
+            if (!participants.Any())
+            {
+                return NotFound("No participants found for this event.");
+            }
+
+            // Map participants to DTO
+            var result = participants.Select(p => new ParticipantResponce
+            {
+                Id = p.User.Id,
+                Name = p.User.Name,
+                Email = p.User.Email
+            });
+
+            return Ok(result);
+        }
+
+       
+
+        
+    }
+    public class ParticipantResponce
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
